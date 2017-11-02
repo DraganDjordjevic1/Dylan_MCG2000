4b20f8aa517e2858b765e230c68868d36c64f2f0
DraganDjordjevic1 <101023448@student.swin.edu.au>
2017-11-02 23:19:19 +1100

    Initial commit

---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Swinburne University")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright © Swinburne University 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1d0b5c9c-5fc8-414e-bb5b-05e81879c6e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/UnitTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void ChoosePlayerClass()
+        {
+            GameController vTest = new GameController(PlayerCharacter, NonPlayerCharacter);
+            string vPlayerClass = vTest.ChoosePlayerClass("Thief");
+            vPlayerClass = "Warrior";
+           
+        }
+        [TestMethod]
+        public void startBattle()
+        {
+            GameController vTest = new GameController(PlayerCharacter, NonPlayerCharacter);
+            vTest.startBattle();
+            vTest.startBattle();
+
+        }
+        [TestMethod]
+        public void normalAttack()
+        {
+            GameController vTest = new GameController(PlayerCharacter, NonPlayerCharacter);
+            vTest.normalAttack();
+            vTest.normalAttack();
+
+        }
+        [TestMethod]
+        public void specialAttack()
+        {
+            GameController vTest = new GameController(PlayerCharacter, NonPlayerCharacter);
+            vTest.specialAttack();
+            Assert.AreEqual(vTest.specialAttack());
+
+        }
+        [TestMethod]
+        public void getNormalAttackDamage()
+        {
+            PlayerCharacter vTest = new PlayerCharacter(10);
+            int vResult = vTest.getNormalAttackDamage(10);
+            vResult = 10;
+
+        }
+        [TestMethod]
+        public void calcSpecialAttack()
+        {
+            PlayerCharacter vTest = new PlayerCharacter("Player", specialAttack);
+            bool vResult = vTest.calcSpecialAttack(true);
+            vResult = 10;
+
+        }
+        [TestMethod]
+        public void getSpecialAttackDamage()
+        {
+            PlayerCharacter vTest = new PlayerCharacter(20, Warrior);
+            int vResult = vTest.getSpecialAttackDamage(20);
+            vResult = 20;
+
+        }
+        [TestMethod]
+        public void specialAttackFailure()
+        {
+            PlayerCharacter vTest = new PlayerCharacter("Missed Turn");
+            vTest.specialAttackFailure();
+            vTest.specialAttackFailure();
+
+        }
+        [TestMethod]
+        public void damageSelf()
+        {
+            PlayerCharacter vTest = new PlayerCharacter(10);
+            int vResult = vTest.damageSelf(10); 
+            vResult = 10;
+
+        }
+        [TestMethod]
+        public void takeDamage()
+        {
+            NonPlayerCharacter vTest = new NonPlayerCharacter(10, 20);
+            int vResult = vTest.takeDamage(10, 20);
+            vResult = 10;
+
+        }
+
+
+    }
+}
